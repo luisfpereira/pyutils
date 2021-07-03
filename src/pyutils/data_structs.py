f3a2b296d57793e865b86f2a60cb5a3ba142ff3b
L. F. Pereira <luisfgpereira95@gmail.com>
2021-07-03 20:49:49 +0200

    Add method to recursively unravel array

--- src/pyutils/data_structs.py
+++ src/pyutils/data_structs.py
@@ -13,3 +13,17 @@ def transform_into_dict(items, id_key='id', remove_id=True):
             del item[id_key]
 
     return items_dict
+
+
+def unravel_array(array):
+    collector = []
+    _unravel_array(array, collector)
+    return collector
+
+
+def _unravel_array(array, collector):
+    if type(array) in [list, tuple]:
+        for item in array:
+            _unravel_array(item, collector)
+    else:
+        collector.append(array)
