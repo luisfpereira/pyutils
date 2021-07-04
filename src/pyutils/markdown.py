618dadb2ecc5791d8ad731b8eb1ff50300e69248
L. F. Pereira <luisfgpereira95@gmail.com>
2021-07-04 12:05:41 +0200

    Update markdown function names

--- src/pyutils/markdown.py
+++ src/pyutils/markdown.py
@@ -1,21 +1,21 @@
 import re
 
-from pyutils.regex_lib import regex_lib
+from pyutils import regex_lib
 
 
-def increase_markdown_header(text):
+def increase_header(text):
     regex = re.compile(regex_lib.markdown_headers_symbol())
 
     return regex.sub(lambda m: m.group() + '#', text)
 
 
-def decrease_markdown_header(text):
+def decrease_header(text):
     regex = re.compile(regex_lib.markdown_headers_symbol())
 
     return regex.sub(lambda m: m.group()[:-1], text)
 
 
-def get_markdown_headers(text, level):
+def get_headers(text, level):
     regex = re.compile(regex_lib.markdown_headers(level=level))
 
     return regex.findall(text)
