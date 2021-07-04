1bf63c61c7cd1bba0c75f121eec2abc2b28e13bb
L. F. Pereira <luisfgpereira95@gmail.com>
2021-07-04 12:22:57 +0200

    Update markdown_headers regex

--- src/pyutils/regex_lib.py
+++ src/pyutils/regex_lib.py
@@ -1,4 +1,4 @@
-'''Library of regex expressions. I've chosen to represent them as functions to
+"""Library of regex expressions. I've chosen to represent them as functions to
 make use of docstrings, otherwise they become unusable really fast (as they
 are unintelligible immediately after I develop them).
 
@@ -6,7 +6,7 @@ Notes
 -----
 * function names, exceptionally, do not follow good practice of starting by a
 verb.
-'''
+"""
 
 
 def url():
@@ -14,34 +14,42 @@ def url():
 
 
 def python_imports():
-    '''Returns python import in groups, considering the general statement
+    """Returns python import in groups, considering the general statement
     `from <g1> import <g2> as <g3>`
-    '''
+    """
     # TODO: deal with multiple imports from the same place
     return r'(?:from[ ]+)*(\S{1,})*[ ]*import[ ]+(\S{1,})(?:[ ]+as[ ]+)*(\S{1,})*'
 
 
 def punctuation(chars=',.\"!@#$%^&*(){}?/;`~:<>+=-'):
-    '''Finds characters in text. Useful to preprocess text. Do not forget
+    """Finds characters in text. Useful to preprocess text. Do not forget
     to escape special characters.
-    '''
+    """
     return rf'[{chars}]'
 
 
 def extra_whitespaces():
-    '''Specially useful to remove extra whitespaces from text. Just replace
+    """Specially useful to remove extra whitespaces from text. Just replace
     by an empty str.
-    '''
+    """
     return r'(?<=[ ]{1})[ ]{1,}'
 
 
 def markdown_headers_symbol():
-    '''Get markdown headers symbol (to get or change level).
-    '''
+    """Get markdown headers symbol (to get or change level).
+    """
     return r'^[#]{1,}|(?<=\n)[#]{1,}'
 
 
-def markdown_headers(level):
-    '''Get markdown headers (without symbol).
-    '''
-    return rf'(?<=^[#]{{{level}}} |(?<=\n)[#]{{{level}}} )[^\t\n]+'
+def markdown_headers(level=None):
+    """Get markdown headers (without symbol).
+    """
+    if level is None:
+        return r'(?:^[#]+ |\n[#]+ )([^\t\n]+)'
+    else:
+        return rf'(?:^[#]{{{level}}} |\n[#]{{{level}}} )([^\t\n]+)'
+
+
+
+
+
