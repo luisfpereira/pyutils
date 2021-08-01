3cad392b83f1f564caf8e015e92e6c0576c625f7
L. F. Pereira <luisfgpereira95@gmail.com>
2021-08-01 20:55:01 +0200

    Add first binance utils

---
+++ src/pyutils/binance.py
@@ -0,0 +1,41 @@
+from pathlib import Path
+import json
+
+from web_apis.binance.sessions import SignedBinanceSession
+from web_apis.binance.queries import AccountSnapshotQuery
+
+
+# TODO: very similar to joplin. make abstract?
+def create_signed_session():
+    secrets = get_secrets()
+    return SignedBinanceSession(**secrets)
+
+
+def get_account_balance_details(session):
+    query = AccountSnapshotQuery()
+    req = session.get(query)
+    data = req.json()
+
+    # get most recent snapshot
+    snapshot = data['snapshotVos'][-1]
+
+    return {asset['asset']: asset['free'] for asset in snapshot['data']['balances']}
+
+
+# TODO: get average price of buying
+# TODO: get eur estimation
+# TODO: get total deposit
+# TODO: plot losses/gains (deposit - current eur balance)
+# TODO: create bar plot with current losses for each coin
+
+
+def get_auth_filepath():
+    return Path.home() / '.pyutils' / 'binance_secrets.json'
+
+
+def get_secrets():
+    filepath = get_auth_filepath()
+    with open(filepath, 'r') as file:
+        data = json.load(file)
+
+    return data
