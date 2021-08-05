07404739eb702d1f358e21d3a76b84448ad752c7
L. F. Pereira <luisfgpereira95@gmail.com>
2021-08-05 14:37:44 +0200

    Update verification of tests run by existence of non-empty nodeids file

--- src/pyutils/pytest.py
+++ src/pyutils/pytest.py
@@ -9,7 +9,7 @@ def have_tests_failed(dir_name):
 
             - 0: Tests were successful.
             - 1: Tests failed.
-            - 2: pycache directory does not exist.
+            - 2: pycache directory does not exist or tests were not performed.
     """
     # TODO: robustness still to be confirmed
 
@@ -22,14 +22,24 @@ def have_tests_failed(dir_name):
     if not pycache_path.exists():
         return 2
 
+    # py cache exists, but tests were not run
+    run_info = _read_info_from_json(list(pycache_path.glob('**/nodeids'))[0])
+    if len(run_info) == 0:
+        return 2
+
     # lastfailed file does not exist
     lastfailed_paths = list(pycache_path.glob('**/lastfailed'))
     if len(lastfailed_paths) == 0:
         return 0
 
     # lastfailed file exists
-    lastfailed_path = lastfailed_paths[0]
-    with open(lastfailed_path, 'r') as file:
-        fail_info = json.load(file)
+    fail_info = _read_info_from_json(lastfailed_paths[0])
 
     return int(len(fail_info) > 0)
+
+
+def _read_info_from_json(filename):
+    with open(filename, 'r') as file:
+        info = json.load(file)
+
+    return info
