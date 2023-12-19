547fa2f9c228d04f0f022a36a6415a61ef6ace61
L. F. Pereira <luisfgper@gmail.com>
2023-12-19 11:57:45 +0100

    Update tests

--- tests/test_markdown.py
+++ tests/test_markdown.py
@@ -12,32 +12,35 @@ from pyutils.markdown import get_headers
 
 def test_markdown_level():
 
-    scenarios = ['# This is a header\n',
-                 '## this is another, but not # this']
+    scenarios = ["# This is a header\n", "## this is another, but not # this"]
 
-    text = ''.join(scenarios)
+    text = "".join(scenarios)
 
     # increase level
-    expected_output = '## This is a header\n### this is another, but not # this'
+    expected_output = "## This is a header\n### this is another, but not # this"
     output = increase_header(text)
     assert output == expected_output
 
     # decrease level
-    expected_output = 'This is a header\n# this is another, but not # this'
+    expected_output = "This is a header\n# this is another, but not # this"
     output = decrease_header(text).lstrip()
     assert output == expected_output
 
 
 def test_markdown_headers():
-    scenarios = ['# This is a header\n',
-                 '# This is the second one\n',
-                 '## this is another, but not # this']
+    scenarios = [
+        "# This is a header\n",
+        "# This is the second one\n",
+        "## this is another, but not # this",
+    ]
 
-    text = ''.join(scenarios)
+    text = "".join(scenarios)
 
     # level
-    expected_outputs = [['This is a header', 'This is the second one'],
-                        ['this is another, but not # this']]
+    expected_outputs = [
+        ["This is a header", "This is the second one"],
+        ["this is another, but not # this"],
+    ]
 
     for i, expected_output in enumerate(expected_outputs):
         output = get_headers(text, i + 1)
--- tests/test_regex_lib.py
+++ tests/test_regex_lib.py
@@ -7,13 +7,17 @@ from pyutils import regex_lib
 
 def test_url():
 
-    scenarios = ['this is a url https://github.com/\n',
-                 '[joplin style](https://docs.python.org/3/howto/regex.html)']
+    scenarios = [
+        "this is a url https://github.com/\n",
+        "[joplin style](https://docs.python.org/3/howto/regex.html)",
+    ]
 
-    expected_output = ['https://github.com/',
-                       'https://docs.python.org/3/howto/regex.html']
+    expected_output = [
+        "https://github.com/",
+        "https://docs.python.org/3/howto/regex.html",
+    ]
 
-    text = ''.join(scenarios)
+    text = "".join(scenarios)
     regex = re.compile(regex_lib.url())
 
     output = regex.findall(text)
@@ -23,19 +27,23 @@ def test_url():
 
 def test_python_imports():
 
-    scenarios = ['import fitz\n',
-                 'from PIL import Image\n',
-                 'import io\n',
-                 'import matplotlib.pyplot\n',
-                 'from  matplotlib   import pyplot  as plt']
+    scenarios = [
+        "import fitz\n",
+        "from PIL import Image\n",
+        "import io\n",
+        "import matplotlib.pyplot\n",
+        "from  matplotlib   import pyplot  as plt",
+    ]
 
-    expected_output = [('', 'fitz', ''),
-                       ('PIL', 'Image', ''),
-                       ('', 'io', ''),
-                       ('', 'matplotlib.pyplot', ''),
-                       ('matplotlib', 'pyplot', 'plt')]
+    expected_output = [
+        ("", "fitz", ""),
+        ("PIL", "Image", ""),
+        ("", "io", ""),
+        ("", "matplotlib.pyplot", ""),
+        ("matplotlib", "pyplot", "plt"),
+    ]
 
-    text = ''.join(scenarios)
+    text = "".join(scenarios)
     regex = re.compile(regex_lib.python_import())
 
     output = regex.findall(text)
@@ -45,31 +53,32 @@ def test_python_imports():
 
 def test_extra_whitespace():
 
-    text = 'Too much  white   spaces.\n\tbut keep    breaklines and tabs'
-    expected_output = 'Too much white spaces.\n\tbut keep breaklines and tabs'
+    text = "Too much  white   spaces.\n\tbut keep    breaklines and tabs"
+    expected_output = "Too much white spaces.\n\tbut keep breaklines and tabs"
 
     regex = re.compile(regex_lib.extra_whitespaces())
 
-    output = regex.sub('', text)
+    output = regex.sub("", text)
 
     assert output == expected_output
 
 
 def test_punctuation():
-    scenarios = ['Removing urls such as (https://github.com/).\n',
-                 'And $ # % `punctuation`']
+    scenarios = [
+        "Removing urls such as (https://github.com/).\n",
+        "And $ # % `punctuation`",
+    ]
 
-    expected_output = 'Removing urls such as \nAnd punctuation'
+    expected_output = "Removing urls such as \nAnd punctuation"
 
-    text = ''.join(scenarios)
+    text = "".join(scenarios)
 
-    exprs = [regex_lib.url(), regex_lib.punctuation(),
-             regex_lib.extra_whitespaces()]
+    exprs = [regex_lib.url(), regex_lib.punctuation(), regex_lib.extra_whitespaces()]
     subs_regexes = [re.compile(expr) for expr in exprs]
 
     output = text
     for subs_regex in subs_regexes:
-        output = subs_regex.sub('', output)
+        output = subs_regex.sub("", output)
 
     assert output == expected_output
 
@@ -78,12 +87,11 @@ def test_markdown_headers_symbol():
 
     regex = re.compile(regex_lib.markdown_headers_symbol())
 
-    scenarios = ['# This is a header\n',
-                 '## this is another, but not # this']
+    scenarios = ["# This is a header\n", "## this is another, but not # this"]
 
-    expected_output = ['#', '##']
+    expected_output = ["#", "##"]
 
-    text = ''.join(scenarios)
+    text = "".join(scenarios)
 
     output = regex.findall(text)
 
